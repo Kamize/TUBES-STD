c6ee18951d0d1532ff41bc8fd761e0f20cfa6cee
Talitha Nabila <73777177+Kamize@users.noreply.github.com>
2021-12-23 20:34:39 +0700

    ini semuanya

---
+++ rumahsakit.h
@@ -0,0 +1,70 @@
+#ifndef RUMAHSAKIT_H_INCLUDED
+#define RUMAHSAKIT_H_INCLUDED
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+#define info(P) (P) -> info
+#define next(P) (P) -> next
+#define nextPasien(P) (P) -> nextPasien
+#define first(L) ((L).first)
+#define head(L) ((L).head)
+#define tail(L) ((L).tail)
+#define NIL NULL
+
+// [PARENT] DLL untuk dokter spesialis
+typedef struct rumahSakit *adr_ruangan;
+
+struct ruangan{
+    string dokter, spesialisasi, waktu;
+};
+
+struct rumahSakit{
+    ruangan info;
+    adr_ruangan next, prev;
+    adr_pasien nextPasien;
+};
+
+struct mll{
+    adr_ruangan first;
+}
+
+// [CHILD] SLL / Queue untuk pasien
+typedef struct antrian *adr_pasien;
+
+struct pasien {
+    string nama, idPasien, keluhan;
+    int umur;
+};
+
+struct antrian {
+    pasien info;
+    adr_pasien next,head,tail;
+};
+
+//contract function [INSERT DATA] / CREATE
+void create_list(mll &RS);
+adr_ruangan newElm_ruangan(ruangan info);
+adr_pasien newElm_pasien(pasien info);
+void insertLast_ruangan(mll &RS, adr_ruangan R);
+
+//ENQUEUE
+void insertNew_pasien(mll &RS, adr_ruangan R, adr_pasien P);
+
+//DEQUEUE
+void proses_pasien(mll &RS, adr_ruangan R, adr_pasien P);
+
+//DELETE
+void deleteFirst_ruangan(mll &RS, adr_ruangan R);
+void deleteLast_ruangan(mll &RS, adr_ruangan R);
+void deleteAfter_ruangan(mll &RS, adr_ruangan R);
+void delete_ruangan(mll &RS);
+
+//SHOW
+void showData_RS(mll RS);
+
+//SEARCH
+adr_pasien search_pasien(mll RS, string Name);
+
+#endif // RUMAHSAKIT_H_INCLUDED
